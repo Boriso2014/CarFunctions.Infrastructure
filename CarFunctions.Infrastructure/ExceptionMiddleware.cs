14f8455f229611af3a9e2f9b0a38a7841f4b1898
Boris Obuhov <b.obuhov@outlook.com>
2022-04-15 10:26:58 +0300

    Add project files.

---
+++ CarFunctions.Infrastructure/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+
+namespace CarFunctions.Infrastructure
+{
+    internal sealed class ExceptionMiddleware : IFunctionsWorkerMiddleware
+    {
+        private readonly ILogger _logger;
+
+        public ExceptionMiddleware(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
+        }
+
+        public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (AggregateException aex)
+            {
+                _logger.LogError("Unexpected Error in {EntryPoint}", context.FunctionDefinition.EntryPoint);
+                foreach (var ex in aex.InnerExceptions)
+                {
+                    _logger.LogError("Request failed:{NewLine}{Message}", Environment.NewLine, ex.Message);
+                }
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unexpected Error in {EntryPoint}: {Message}", context.FunctionDefinition.EntryPoint, ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ CarFunctions.Infrastructure/ExceptionMiddlewareRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+
+namespace CarFunctions.Infrastructure
+{
+    public static class ExceptionMiddlewareRegistration
+    {
+        public static void UseExceptionHandling(this IFunctionsWorkerApplicationBuilder builder)
+        {
+            builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
